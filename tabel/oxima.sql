3e639769dd6b5c9b9c12a8cfea47130ec8b357eb
yovie <yoviesmanda@gmail.com>
2014-11-18 06:26:21 +0700

    table

--- tabel/oxima.sql
+++ tabel/oxima.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 17, 2014 at 06:43 AM
+-- Generation Time: Nov 18, 2014 at 07:26 AM
 -- Server version: 5.5.35
 -- PHP Version: 5.4.4-14+deb7u10
 
@@ -23,6 +23,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `groups`
+--
+
+CREATE TABLE IF NOT EXISTS `groups` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `group` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `groups`
+--
+
+INSERT INTO `groups` (`id`, `group`) VALUES
+(1, 'Admin'),
+(2, 'Operator'),
+(3, 'Member');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `menus`
 --
 
@@ -32,12 +53,46 @@ CREATE TABLE IF NOT EXISTS `menus` (
   `position` varchar(10) NOT NULL,
   `groups` varchar(10) NOT NULL DEFAULT '*',
   `label` varchar(70) NOT NULL,
-  `controller` varchar(40) NOT NULL,
-  `action` varchar(40) NOT NULL,
-  `routing` text NOT NULL,
+  `module_id` int(11) NOT NULL,
+  `param` varchar(30) NOT NULL,
   `show` tinyint(4) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data for table `menus`
+--
+
+INSERT INTO `menus` (`id`, `menu_id`, `position`, `groups`, `label`, `module_id`, `param`, `show`) VALUES
+(1, 0, 'topleft', '*', 'Beranda', 1, '', 1),
+(2, 0, 'topleft', '*', 'Profil', 0, '', 1),
+(3, 2, 'topleft', '*', 'Profil Perusahaan', 0, '', 1),
+(4, 2, 'topleft', '*', 'Profil Produk', 0, '', 1),
+(5, 0, 'topright', '*', 'Settings', 0, '', 1),
+(6, 0, 'topright', '*', 'Logout', 0, '', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `modules`
+--
+
+CREATE TABLE IF NOT EXISTS `modules` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `controller` varchar(50) NOT NULL,
+  `action` varchar(50) NOT NULL,
+  `routes` text NOT NULL,
+  `params` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `modules`
+--
+
+INSERT INTO `modules` (`id`, `controller`, `action`, `routes`, `params`) VALUES
+(1, 'welcome', 'index', 'beranda', ''),
+(2, 'welcome', 'test', 'ambil-angka/(:any)', '$1');
 
 -- --------------------------------------------------------
 
@@ -51,7 +106,14 @@ CREATE TABLE IF NOT EXISTS `settings` (
   `setupvalue` text NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `setupkey` (`setupkey`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `settings`
+--
+
+INSERT INTO `settings` (`id`, `setupkey`, `setupvalue`) VALUES
+(1, 'LAYOUT', 'default');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
