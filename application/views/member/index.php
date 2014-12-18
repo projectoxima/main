e22165a637b676bbf4120953ef21b03320364b09
MTaufik <MTaufik@Outlook.com>
2014-12-19 05:52:42 +0700

    JointJS

--- application/views/member/index.php
+++ application/views/member/index.php
@@ -1,18 +1,3 @@
-<link href="<?php echo site_url(); ?>/assets/js/orgchart/jquery.orgchart.css" rel="stylesheet">
-<link href="<?php echo site_url(); ?>/assets/js/orgchart/custom.css" rel="stylesheet">
-<style type="text/css">
-    .jsAction:hover {
-        cursor: pointer;
-        color: #00f;
-        text-decoration: underline;
-    }
-
-    .people:hover {
-        cursor: pointer;
-        text-decoration: underline;
-    }
-</style>
-
 <a class="btn btn-primary" href="<?php echo base_url() ;?>member/add_member">Add Member</a>
 <br><br>
 <span for="username" class="error-span"><?php echo $this->session->flashdata('success'); ?></span>
@@ -97,10 +82,8 @@
   	<a class="btn btn-primary" href="<?php echo base_url() ;?>member/">Member Tree</a>
   	<br>
 
-		<div class="hide"><?php echo $tree; ?></div>
-		<div id="orgchart-container"></div>
+		<div id="paper"></div>
 
-		<div id="orgchart"></div>
   </div>
 </div>
 
@@ -136,77 +119,55 @@
     </div><!-- /.modal-content -->
   </div><!-- /.modal-dialog -->
 </div><!-- /.modal -->
-<script src="<?php echo site_url(); ?>/assets/js/orgchart/jquery.orgchart.js"></script>
 <script type="text/javascript">
-	$(document).ready(function(){
-		$('#org-chart').orgChart({
-		  container: $('#orgchart-container')
-		  /**,
-		  nodeClicked: function($node){
-		    var id = $node.data('id');
-		    $.ajax({
-		      url: '<?php echo base_url();?>member/get_profile/' + id,
-		      dataType: 'json'
-		    })
-		    .done(function(response, textStatus, jqhr){
-		      if(response.status == "ok"){
-		        $('#view-name').val(response.data.nama_lengkap);
-		        $('#view-alamat').val(response.data.alamat);
-		        $('#titik-id').val(id);
-		      }
-		    })
-		    .fail(function(){
-		    	alert('fail');
-		    });
+	var graph = new joint.dia.Graph;
 
-		    $('#modal-view-profile').modal('show')
-		  }
-		  **/
-		});
-
-		$('#upline-button').click(function(){
-			var id = $('#titik-id').val();
-			window.location = '<?php echo base_url(); ?>' + 'member/upline/' + id;
-		});
-
-		$('.sample').click(function(){
-			var id = $(this).attr('object');
-			$.ajax({
-			  url: '<?php echo base_url();?>member/get_profile/' + id,
-			  dataType: 'json'
-			})
-			.done(function(response, textStatus, jqhr){
-			  if(response.status == "ok"){
-			    $('#view-name').val(response.data.nama_lengkap);
-			    $('#view-alamat').val(response.data.alamat);
-			    $('#titik-id').val(id);
-			  }
-			})
-			.fail(function(){
-				alert('fail');
-			});
-
-			$('#modal-view-profile').modal('show');
-		});
-
-		$('.people').click(function(){
-			var id = $(this).attr('object');
-			$.ajax({
-			  url: '<?php echo base_url();?>member/get_profile/' + id,
-			  dataType: 'json'
-			})
-			.done(function(response, textStatus, jqhr){
-			  if(response.status == "ok"){
-			    $('#view-name').val(response.data.nama_lengkap);
-			    $('#view-alamat').val(response.data.alamat);
-			    $('#titik-id').val(id);
-			  }
-			})
-			.fail(function(){
-				alert('fail');
-			});
-
-			$('#modal-view-profile').modal('show');
-		});
+	var paper = new joint.dia.Paper({
+	    el: $('#paper'),
+	    width: 800,
+	    height: 600,
+	    gridSize: 1,
+	    model: graph,
+	    perpendicularLinks: true
 	});
+
+	var member = function(x, y, rank, name, image, background, border) {
+
+	    var cell = new joint.shapes.org.Member({
+	        position: { x: x, y: y },
+	        attrs: {
+	            '.card': { fill: background, stroke: border},
+	              image: { 'xlink:href': '<?php echo site_url() ;?>assets/img/'+ image },
+	            '.rank': { text: rank }, '.name': { text: name }
+	        }
+	    });
+	    graph.addCell(cell);
+	    return cell;
+	};
+
+	function link(source, target, breakpoints) {
+
+	    var cell = new joint.shapes.org.Arrow({
+	        source: { id: source.id },
+	        target: { id: target.id },
+	        vertices: breakpoints
+	    });
+	    graph.addCell(cell);
+	    return cell;
+	}
+
+	var bart = member(300,70,'CEO', 'Bart Simpson', 'member1.png', '#F1C40F', 'gray');
+	var homer = member(90,200,'VP Marketing', 'Homer Simpson', 'member2.png', '#2ECC71', '#008e09');
+	var marge = member(300,200,'VP Sales', 'Marge Simpson', 'member3.png', '#2ECC71', '#008e09');
+	var lisa = member(500,200,'VP Production' , 'Lisa Simpson', 'member4.png', '#2ECC71', '#008e09');
+	var maggie = member(400,350,'Manager', 'Maggie Simpson', 'member5.png', '#3498DB', '#333');
+	var lenny = member(190,350,'Manager', 'Lenny Leonard', 'member6.png', '#3498DB', '#333');
+	var carl = member(190,500,'Manager', 'Carl Carlson', 'member7.png', '#3498DB', '#333');
+
+	link(bart, marge, [{x: 385, y: 180}]);
+	link(bart, homer, [{x: 385, y: 180}, {x: 175, y: 180}]);
+	link(bart, lisa, [{x: 385, y: 180}, {x: 585, y: 180}]);
+	link(homer, lenny, [{x:175 , y: 380}]);
+	link(homer, carl, [{x:175 , y: 530}]);
+	link(marge, maggie, [{x:385 , y: 380}]);
 </script>
